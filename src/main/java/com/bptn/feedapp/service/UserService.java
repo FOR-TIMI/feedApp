bfa792148e5aac96e73bbca321563458429035d0
FOR-TIMI <simifemi-ladiran@localhost>
2023-03-07 15:27:44 -0500

    Added User service file

---
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -0,0 +1,23 @@
+package com.bptn.feedapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedapp.jdbc.UserDao;
+import com.bptn.feedapp.jdbc.UserBean;
+
+@Service
+public class UserService {
+	@Autowired
+	UserDao userDao;
+	
+	public List<UserBean> listUsers() {
+		return this.userDao.listUsers();
+	}
+
+	public void createUser(UserBean user) {
+		this.userDao.createUser(user);
+	}
+}
