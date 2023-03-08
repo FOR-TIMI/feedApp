de371394604e3872613a0ba726215b17968b6610
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-08 14:55:24 -0500

    Added User Repository

---
+++ src/main/java/com/bptn/feedapp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bptn.feedapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bptn.feedapp.jpa.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmailId(String email);
+}
