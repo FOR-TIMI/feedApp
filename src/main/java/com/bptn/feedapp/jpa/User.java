e123d51811badbebcd443aaeffd0a887e9a7ad52
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-08 14:39:44 -0500

    Added User JPA Entity

---
+++ src/main/java/com/bptn/feedapp/jpa/User.java
@@ -0,0 +1,132 @@
+package com.bptn.feedapp.jpa;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="\"User\"")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="\"userId\"")
+	private Integer userId;
+	
+	@Column(name="\"firstName\"")
+	private String firstName;
+	
+	@Column(name="\"lastName\"")
+	private String lastName;
+	
+	@Column(name="\"username\"")
+	private String username;
+	
+	@Column(name="\"password\"")
+	private String password;
+	
+	@Column(name="\"phone\"")
+	private String phone;
+	
+	@Column(name="\"emailId\"")
+	private String emailId;
+	
+	@Column(name="\"emailVerified\"")
+	private Boolean emailVerified;
+	
+	@Column(name="\"createdOn\"")
+	private Timestamp createdOn;
+	
+	public User() {
+		
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Boolean getEmailVerified() {
+		return emailVerified;
+	}
+
+	public void setEmailVerified(Boolean emailVerified) {
+		this.emailVerified = emailVerified;
+	}
+
+	public Timestamp getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Timestamp createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
+				+ username + ", password=" + password + ", phone=" + phone + ", emailId=" + emailId + ", emailVerified="
+				+ emailVerified + ", createdOn=" + createdOn + "]";
+	}
+	
+	
+}
