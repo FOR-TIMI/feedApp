2540153ca1ba3f0577d207cb0773b96edbb61d8c
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-10 10:46:15 -0500

    Added email dependency and fixed "profileId" column error

--- src/main/java/com/bptn/feedapp/jpa/Profile.java
+++ src/main/java/com/bptn/feedapp/jpa/Profile.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Profile implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="\"profileId\"")
 	@JsonProperty(access = Access.WRITE_ONLY)
 	private Integer profileId;
 
