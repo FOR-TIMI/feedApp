11ff97c2ab1b8be46f4ce29aa750796e1b507917
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-10 13:36:43 -0500

    Added signup method in User controller

--- src/main/java/com/bptn/feedapp/controller/UserController.java
+++ src/main/java/com/bptn/feedapp/controller/UserController.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,6 +66,13 @@ public class UserController {
 		this.userService.createUser(user);
 
 		return "User Created Successfully";
+	}
+	
+	@PostMapping("/signup")
+	public User signup(@RequestBody User user) {
+		
+		logger.debug("Signing up, username: {}", user.getUsername());
+		return this.userService.signup(user);
 	}
 
 }
