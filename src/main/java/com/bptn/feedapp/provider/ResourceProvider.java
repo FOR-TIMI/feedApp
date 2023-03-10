7e4cdc48d867f55224103b46e8bf68ab71ea9eb4
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-10 14:09:03 -0500

    Added JWT configurations

---
+++ src/main/java/com/bptn/feedapp/provider/ResourceProvider.java
@@ -0,0 +1,92 @@
+package com.bptn.feedapp.provider;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import com.bptn.feedapp.provider.factory.YamlPropertySourceFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+@PropertySource(value = "classpath:config.yml", factory = YamlPropertySourceFactory.class)
+public class ResourceProvider {
+
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+
+	@Value("${jwt.expiration}")
+	private long jwtExpiration;
+
+	@Value("${jwt.issuer}")
+	private String jwtIssuer;
+
+	@Value("${jwt.audience}")
+	private String jwtAudience;
+
+	@Value("${jwt.prefix}")
+	private String jwtPrefix;
+
+	@Value("${jwt.excluded.urls}")
+	private String[] jwtExcludedUrls;
+
+	@Value("${client.url}")
+	private String clientUrl;
+
+	@Value("${client.email.verify.param}")
+	private String clientVerifyParam;
+
+	@Value("${client.email.verify.expiration}")
+	private long clientVerifyExpiration;
+
+	@Value("${client.email.reset.param}")
+	private String clientResetParam;
+
+	@Value("${client.email.reset.expiration}")
+	private long clientResetExpiration;
+
+	public String getJwtSecret() {
+		return jwtSecret;
+	}
+
+	public long getJwtExpiration() {
+		return jwtExpiration;
+	}
+
+	public String getJwtIssuer() {
+		return jwtIssuer;
+	}
+
+	public String getJwtAudience() {
+		return jwtAudience;
+	}
+
+	public String getJwtPrefix() {
+		return jwtPrefix;
+	}
+
+	public String[] getJwtExcludedUrls() {
+		return jwtExcludedUrls;
+	}
+
+	public String getClientUrl() {
+		return clientUrl;
+	}
+
+	public String getClientVerifyParam() {
+		return clientVerifyParam;
+	}
+
+	public long getClientVerifyExpiration() {
+		return clientVerifyExpiration;
+	}
+
+	public String getClientResetParam() {
+		return clientResetParam;
+	}
+
+	public long getClientResetExpiration() {
+		return clientResetExpiration;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/bptn/feedapp/provider/factory/YamlPropertySourceFactory.java
@@ -0,0 +1,31 @@
+package com.bptn.feedapp.provider.factory;
+
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+    @Override
+    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) throws IOException {
+        Properties loadedProperties = this.loadYamlIntoProperties(resource.getResource());
+        
+        return new PropertiesPropertySource((StringUtils.hasLength(name)) ? name : resource.getResource().getFilename(), loadedProperties);
+    }
+
+    private Properties loadYamlIntoProperties(Resource resource) {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(resource);
+        factory.afterPropertiesSet();
+        
+        return factory.getObject();
+    }
+}
