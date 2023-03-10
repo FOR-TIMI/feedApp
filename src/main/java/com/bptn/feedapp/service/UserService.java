51dac05bfbefba5801bd7f455d9807916bc84a99
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-10 15:13:04 -0500

    Updated user service to send email verification after sign up

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -16,6 +16,9 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
+	    
+	@Autowired
+	EmailService emailService;
 	
 	public User signup(User user) {
 		user.setUsername(user.getUsername().toLowerCase());
@@ -23,6 +26,7 @@ public class UserService {
 		user.setEmailVerified(false);
 		user.setCreatedOn(Timestamp.from(Instant.now()));
 		this.userRepository.save(user);
+		this.emailService.sendVerificationEmail(user);
 		
 		return user;
 	}
