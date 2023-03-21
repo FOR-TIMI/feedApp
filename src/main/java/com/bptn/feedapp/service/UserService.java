efd876de210a0e1b801de331379665bf4b636bbc
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-21 11:29:14 -0400

    Added methods to check if a user is verified, authenticate and generate HttpHeaders for user

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -6,15 +6,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.bptn.feedapp.jpa.User;
-import com.bptn.feedapp.repository.UserRepository;
 import com.bptn.feedapp.exception.domain.EmailExistException;
-import com.bptn.feedapp.exception.domain.UsernameExistException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.bptn.feedapp.exception.domain.EmailNotVerifiedException;
 import com.bptn.feedapp.exception.domain.UserNotFoundException;
+import com.bptn.feedapp.exception.domain.UsernameExistException;
+import com.bptn.feedapp.jpa.User;
+import com.bptn.feedapp.provider.ResourceProvider;
+import com.bptn.feedapp.repository.UserRepository;
+import com.bptn.feedapp.security.JwtService;
 
 @Service
 public class UserService {
@@ -27,8 +35,17 @@ public class UserService {
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	JwtService jwtService;
 	
-	/*To register a user*/
+	@Autowired
+	ResourceProvider provider;
+	
+	/* To register a user */
 	public User signup(User user) {
 
 		user.setUsername(user.getUsername().toLowerCase());
@@ -50,8 +67,8 @@ public class UserService {
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
 	}
-	
-	/* To find a user by their user name if it exists in the database*/
+
+	/* To find a user by their user name if it exists in the database */
 	public Optional<User> findByUsername(String username) {
 		return this.userRepository.findByUsername(username);
 	}
@@ -60,7 +77,7 @@ public class UserService {
 		this.userRepository.save(user);
 	}
 
-	/* To make sure a user name and email is unique*/
+	/* To make sure a user name and email is unique */
 	private void validateUsernameAndEmail(String username, String emailId) {
 
 		this.userRepository.findByUsername(username).ifPresent(u -> {
@@ -72,7 +89,7 @@ public class UserService {
 		});
 
 	}
-	
+
 	/* To verify a user's email */
 	public void verifyEmail() {
 
@@ -85,4 +102,39 @@ public class UserService {
 
 		this.userRepository.save(user);
 	}
+
+	/* To check if a user's email is verified */
+	private static User isEmailVerified(User user) {
+		if (user.getEmailVerified().equals(false)) {
+			throw new EmailNotVerifiedException(String.format("Email requires verification, %s", user.getEmailId()));
+		}
+
+		return user;
+	}
+
+	/* To authenticate user passed in as parameter */
+	private Authentication authenticate(String username, String password) {
+		return this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+	}
+
+	/*To over load the above authenticate method and return the users that have verified their email or throw an error if the user has not verified their email*/
+	public User authenticate(User user) {
+
+		/* Spring security authentication */
+		this.authenticate(user.getUsername(), user.getPassword());
+
+		/* Get user from the DB */
+		return this.userRepository.findByUsername(user.getUsername()).map(UserService::isEmailVerified).get();
+	}
+
+	/*generates a JWT token for a given user and returns the header with a generated token*/
+	public HttpHeaders generateJwtHeader(String username) {
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username, this.provider.getJwtExpiration()));
+		
+		return headers;
+	}
+
+
 }
