95667e51583d4ed465f5e71beb46a442d1a8e77b
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-21 11:48:13 -0400

    Updated User controller to include end point for login

--- src/main/java/com/bptn/feedapp/controller/UserController.java
+++ src/main/java/com/bptn/feedapp/controller/UserController.java
@@ -7,8 +7,11 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import static org.springframework.http.HttpStatus.OK;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.service.UserService;
 
+@CrossOrigin(exposedHeaders = "Authorization")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -68,6 +72,23 @@ public class UserController {
 		return "User Created Successfully";
 	}
 	
+	
+	@PostMapping("/login")
+	public ResponseEntity<User> login(@RequestBody User user){
+		logger.debug("Authenticating, username:{}, password: {}", user.getUsername(), user.getPassword());
+		
+		/* Spring security Authentication */
+		user = this.userService.authenticate(user);
+		
+		/* Generate JWT and HTTP Header */
+		HttpHeaders jwtHeader = this.userService.generateJwtHeader(user.getUsername());
+		
+		logger.debug("User Authenticated, username: {}", user.getUsername());
+		
+		return new ResponseEntity<>(user,jwtHeader, OK);
+	}
+	
+	
 	@PostMapping("/signup")
 	public User signup(@RequestBody User user) {
 		
@@ -80,4 +101,6 @@ public class UserController {
 		logger.debug("Verifying Email");
 		this.userService.verifyEmail();
 	}
+
+	
 }
