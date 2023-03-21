4d5eac2f1f3e38e79811a9de9170a3a55c8c768b
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-21 09:24:10 -0400

    Updated user service signup method to encrypt passwords on signup

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -10,26 +10,39 @@ import org.springframework.stereotype.Service;
 
 import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.repository.UserRepository;
+import com.bptn.feedapp.exception.domain.EmailExistException;
+import com.bptn.feedapp.exception.domain.UsernameExistException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	    
+
 	@Autowired
 	EmailService emailService;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	public User signup(User user) {
+
 		user.setUsername(user.getUsername().toLowerCase());
 		user.setEmailId(user.getEmailId().toLowerCase());
+
+		this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+
 		user.setEmailVerified(false);
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		user.setCreatedOn(Timestamp.from(Instant.now()));
-		this.userRepository.save(user);
+
 		this.emailService.sendVerificationEmail(user);
-		
+
+		this.userRepository.save(user);
 		return user;
-	}
+
+   }
 
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
@@ -41,5 +54,17 @@ public class UserService {
 
 	public void createUser(User user) {
 		this.userRepository.save(user);
+	}
+
+	private void validateUsernameAndEmail(String username, String emailId) {
+
+		this.userRepository.findByUsername(username).ifPresent(u -> {
+			throw new UsernameExistException(String.format("Username already exists, %s", u.getUsername()));
+		});
+
+		this.userRepository.findByEmailId(emailId).ifPresent(u -> {
+			throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
+		});
+
 	}
 }
