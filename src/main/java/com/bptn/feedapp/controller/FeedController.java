d1d07876e3bebc63a8658e6413e5809c6e0b276b
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-23 10:46:28 -0400

    Updated Feed controller to include method to create a feed

---
+++ src/main/java/com/bptn/feedapp/controller/FeedController.java
@@ -0,0 +1,30 @@
+package com.bptn.feedapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bptn.feedapp.jpa.Feed;
+import com.bptn.feedapp.service.FeedService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/feeds")
+public class FeedController {
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	FeedService feedService;
+
+	@PostMapping
+	public Feed createFeed(@RequestBody Feed feed) {
+		logger.debug("Creating Feed");
+
+		return this.feedService.createFeed(feed);
+	}
+}
