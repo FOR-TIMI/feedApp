5cd89429ec030cceef6304067099d7c340c44d43
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-24 11:45:14 -0400

    Added delete feed end point to a FeedController

--- src/main/java/com/bptn/feedapp/controller/FeedController.java
+++ src/main/java/com/bptn/feedapp/controller/FeedController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,12 @@ public class FeedController {
 		logger.debug("Creating FeedMetaData, feedId: {}", feedId);
 
 		return this.feedService.createFeedMetaData(feedId, meta);
+	}
+	
+	@DeleteMapping("/{feedId}")
+	public void deleteFeed(@PathVariable int feedId) {
+		logger.debug("Deleting Feed, feedId: {}", feedId);	
+		this.feedService.deleteFeed(feedId);
 	}
 
 }
