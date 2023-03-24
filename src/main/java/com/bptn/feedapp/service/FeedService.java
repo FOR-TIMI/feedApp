969d22de103bcc6d12a8bb6f2fadaefceee7d18f
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-24 11:33:48 -0400

    Added delete feed method to the feed service

--- src/main/java/com/bptn/feedapp/service/FeedService.java
+++ src/main/java/com/bptn/feedapp/service/FeedService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import com.bptn.feedapp.domain.PageResponse;
 import com.bptn.feedapp.exception.domain.FeedNotFoundException;
+import com.bptn.feedapp.exception.domain.FeedNotUserException;
 import com.bptn.feedapp.exception.domain.LikeExistException;
 import com.bptn.feedapp.exception.domain.UserNotFoundException;
 import com.bptn.feedapp.jpa.Feed;
@@ -124,4 +125,17 @@ public class FeedService {
 		return this.feedMetaDataRepository.save(newMeta);
 	}
 
+	/* To delete a Feed */
+	public void deleteFeed(int feedId) {
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		
+		Feed feed = this.feedRepository.findById(feedId)			
+		             .orElseThrow(()-> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
+
+		Optional.of(feed).filter(f -> f.getUser().getUsername().equals(username))
+			         .orElseThrow(()-> new FeedNotUserException(String.format("Feed doesn't belong to current User, feedId: %d, username: %s", feedId, username)));
+			
+		this.feedRepository.delete(feed);
+	}
 }
